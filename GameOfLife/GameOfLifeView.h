a99caa30566b01c5c7dcf650d3a35c3a82afc75f
Matt Crescimanno <mcrescimanno@gmail.com>
2021-02-22 16:33:24 -0500

    Initial Commit

---
+++ GameOfLife/GameOfLifeView.h
@@ -0,0 +1,12 @@
+//
+//  GameOfLifeView.h
+//  GameOfLife
+//
+//  Created by Matthew Crescimanno on 2/22/21.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface GameOfLifeView : ScreenSaverView
+
+@end
---
+++ GameOfLife/GameOfLifeView.m
@@ -0,0 +1,51 @@
+//
+//  GameOfLifeView.m
+//  GameOfLife
+//
+//  Created by Matthew Crescimanno on 2/22/21.
+//
+
+#import "GameOfLifeView.h"
+
+@implementation GameOfLifeView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
